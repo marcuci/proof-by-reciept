f31ff14f6e9afe006baec3e0b98933a758c7e011
sushant <sushang1@uci.edu>
2023-02-11 22:49:40 -0500

    HOLY SHI

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,50 +1,29 @@
 pragma solidity 0.4.25;
 
 contract Election {
-    // Model a Candidate
+    // Model a Revie
     struct Candidate {
-        uint id;
-        string name;
-        uint voteCount;
+        uint id; //receipt id
+        string name; //revie
+        uint voteCount; //stars
     }
-
-    // Store accounts that have voted
-    mapping(address => bool) public voters;
-    // Store Candidates
-    // Fetch Candidate
-    mapping(uint => Candidate) public candidates;
-    // Store Candidates Count
-    uint public candidatesCount;
-
-    // voted event
-    event votedEvent (
-        uint indexed _candidateId
-    );
+    struct Rev {
+        string name; //this da revie
+        uint stars; // this is the rating
+    
+    }
 
     constructor () public {
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2");
+        addMap(1, "bro no", 2);
+        addMap(2, "YOo", 3);
     }
 
-    function addCandidate (string _name) private {
-        candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    //I want to map the receipt id to the revie and stars
+    // Store accounts that have voted. WE DO NOT NEED TO STORE VOTES.
+    mapping(uint => Rev) public reviewer;
+
+    function addMap(uint _id, string _revi, uint _stars) private {
+        reviewer[_id] = Rev(_revi, _stars);
     }
 
-    function vote (uint _candidateId) public {
-        // require that they haven't voted before
-        require(!voters[msg.sender]);
-
-        // require a valid candidate
-        require(_candidateId > 0 && _candidateId <= candidatesCount);
-
-        // record that voter has voted
-        voters[msg.sender] = true;
-
-        // update candidate vote Count
-        candidates[_candidateId].voteCount ++;
-
-        // trigger voted event
-        emit votedEvent(_candidateId);
-    }
 }
